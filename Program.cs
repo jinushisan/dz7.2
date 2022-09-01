dd3cda7bc5b996273f83bd494886c8c14649fa6d
Egor Antropov <antropov-zhora@mail.ru>
2022-09-01 12:23:23 +0300

    kdhfks

---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿Console.WriteLine("Введите номер строки");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите номер столбца");
+int m = Convert.ToInt32(Console.ReadLine());
+int[,] numbers = new int[5, 5];
+FillArrayRandomNumbers(numbers);
+
+
+
+PrintArray(numbers);
+
+void FillArrayRandomNumbers(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = new Random().Next(-100, 100);
+        }
+    }
+}
+
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        Console.Write("[ ");
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i, j] + " ");
+        }
+        Console.Write("]");
+        Console.WriteLine("");
+    }
+}
+
+if (n > numbers.GetLength(0) || m > numbers.GetLength(1))
+{
+    Console.WriteLine("Такого элемента нет");
+}
+else
+{
+    Console.WriteLine($"Значение элемента {n} строки и столбца {m} равно {numbers[n - 1, m - 1]}");
+}
